ca92e0d72117df7eedc5b02179f72ab8e671b7b0
Rahul keshri <rkeshri2021@gmail.com>
2023-10-25 11:19:08 +0530

    Initial Commit

---
+++ Day1/Intro/server.js
@@ -0,0 +1,7 @@
+let developerName = "Rahul Keshri";
+let developerAge = 23;
+let time = new Date().toISOString();
+
+console.log(
+  `Hi, My name is ${developerName}. I'm ${developerAge} years old. I'm writing this code on ${time}`
+);
